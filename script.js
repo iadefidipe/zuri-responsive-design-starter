e274e551858e46e9c2f433e4059b5081c2f1d950
Israel Adefidipe <israeladefidipe@gmail.com>
2021-04-08 21:07:55 +0100

    hamburger animate done

--- script.js
+++ script.js
@@ -3,16 +3,20 @@
 const btnHamburger = document.querySelector('#btnHamburger')
 const header = document.querySelector('.header') 
 const overLay = document.querySelector('.overlay')
+const hamburger = document.querySelector('.btnHamburger')
 
 btnHamburger.addEventListener('click', function(){
 
     //function will add the open class once the hamburger menu, and remove it when its clicked again 
     if(header.classList.contains('open')){
         header.classList.remove('open');
-        overLay.classList.add('hide-display');
+        overLay.classList.remove('hide-display');
+        hamburger.classList.remove("btnHamburger-animate");
+
     }                                                                                       
     else {
         header.classList.add('open')
-        overLay.classList.remove('hide-display');
+        overLay.classList.add('hide-display');
+        hamburger.classList.add('btnHamburger-animate')
     }
 })
