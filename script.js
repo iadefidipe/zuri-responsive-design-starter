07ea3b570e18678df6a78102c8c26842dc47a16c
Israel Adefidipe <israeladefidipe@gmail.com>
2021-04-08 21:47:22 +0100

    fixed overlay script

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ btnHamburger.addEventListener('click', function(){
     //function will add the open class once the hamburger menu, and remove it when its clicked again 
     if(header.classList.contains('open')){
         header.classList.remove('open');
-        overLay.classList.remove('hide-display');
+        overLay.classList.add('hide-display');
         hamburger.classList.remove("btnHamburger-animate");
 
     }                                                                                       
     else {
         header.classList.add('open')
-        overLay.classList.add('hide-display');
+        overLay.classList.remove('hide-display');
         hamburger.classList.add('btnHamburger-animate')
     }
 })
